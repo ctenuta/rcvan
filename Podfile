ecdfaf17e38c52d538d413afec37f3b2b0139678
ctenuta <cristiano@tenuta.com.br>
2017-02-20 11:48:40 -0300

    Include pod files and project workspace

---
+++ Podfile
@@ -0,0 +1,7 @@
+#platform :ios, '8.0'
+use_frameworks!
+
+target 'RC Van' do
+        pod 'Bean-iOS-OSX-SDK'
+end
+
---
+++ Pods/Target Support Files/Pods-RC Van/Pods-RC Van-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_RC_VanVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RC_VanVersionString[];
+
